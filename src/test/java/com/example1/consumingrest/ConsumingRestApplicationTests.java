808a87a31ec503ea9d82de600f27d28d194df306
Malcolm Cajuste <malcolmacajuste@gmail.com>
2019-10-24 10:11:49 -0400

    Initial commit

---
+++ src/test/java/com/example1/consumingrest/ConsumingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example1.consumingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
